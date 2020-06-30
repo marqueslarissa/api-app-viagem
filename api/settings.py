8eecec340df8f3fbdb0fa37b760ae5403e346269
Larissa Marques da Silva <larissa4.silva@usp.br>
2020-06-29 22:28:37 -0300

    migrate pg db

--- api/settings.py
+++ api/settings.py
@@ -107,7 +107,7 @@ if ON_HEROKU:
 else:
     DATABASE_URL = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
-DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
+DATABASES = {'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600, ssl_require=True)}
 
 # DATABASES = {
 #     'default': {
